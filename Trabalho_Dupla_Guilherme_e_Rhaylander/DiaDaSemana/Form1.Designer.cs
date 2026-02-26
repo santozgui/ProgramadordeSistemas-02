e3f2d76528dfb62d3ec8df803d6d3e3c5e909c26
GUILHERME SANTOS DA ROCHA <guilherme.srocha18@senacsp.edu.br>
2026-02-26 16:48:00 -0300

    trabalho em dupla com rhaylander

---
+++ Trabalho_Dupla_Guilherme_e_Rhaylander/DiaDaSemana/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DiaDaSemana
+{
+    partial class frmDiaSemana
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDiaSemana));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInstrucao = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Moccasin;
+            this.panel1.Controls.Add(this.lblInstrucao);
+            this.panel1.Controls.Add(this.lblResultado);
+            this.panel1.Controls.Add(this.txtNumero);
+            this.panel1.Controls.Add(this.btnVerificar);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 464);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblInstrucao
+            // 
+            this.lblInstrucao.AutoSize = true;
+            this.lblInstrucao.BackColor = System.Drawing.Color.Gold;
+            this.lblInstrucao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInstrucao.Location = new System.Drawing.Point(191, 90);
+            this.lblInstrucao.Name = "lblInstrucao";
+            this.lblInstrucao.Size = new System.Drawing.Size(397, 34);
+            this.lblInstrucao.TabIndex = 4;
+            this.lblInstrucao.Text = "Digite um número de 1 a 7:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoEllipsis = true;
+            this.lblResultado.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblResultado.Location = new System.Drawing.Point(191, 305);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(355, 42);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtNumero.Location = new System.Drawing.Point(191, 157);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(153, 40);
+            this.txtNumero.TabIndex = 2;
+            this.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNumero.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNumero_KeyDown);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.Gold;
+            this.btnVerificar.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificar.Location = new System.Drawing.Point(191, 229);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(342, 51);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 464);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmDiaSemana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 464);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmDiaSemana";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dia da Semana";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblInstrucao;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Trabalho_Dupla_Guilherme_e_Rhaylander/DiaDaSemana/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiaDaSemana
+{
+    public partial class frmDiaSemana : Form
+    {
+        public frmDiaSemana()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            VerificarDiaSemana();
+        }
+
+        private void txtNumero_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                VerificarDiaSemana();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void VerificarDiaSemana()
+        {
+            if (int.TryParse(txtNumero.Text, out int dia))
+            {
+                switch (dia)
+                {
+                    case 1:
+                        lblResultado.Text = "Resultado: Domingo";
+                        break;
+                    case 2:
+                        lblResultado.Text = "Resultado: Segunda-feira";
+                        break;
+                    case 3:
+                        lblResultado.Text = "Resultado: Terça-feira";
+                        break;
+                    case 4:
+                        lblResultado.Text = "Resultado: Quarta-feira";
+                        break;
+                    case 5:
+                        lblResultado.Text = "Resultado: Quinta-feira";
+                        break;
+                    case 6:
+                        lblResultado.Text = "Resultado: Sexta-feira";
+                        break;
+                    case 7:
+                        lblResultado.Text = "Resultado: Sábado";
+                        break;
+                    default:
+                        lblResultado.Text = "Número inválido.";
+                        break;
+                }
+            }
+            else
+            {
+                lblResultado.Text = "Digite apenas números.";
+            }
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
