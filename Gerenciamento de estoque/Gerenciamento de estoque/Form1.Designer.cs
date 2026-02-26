25d7ab3710440b1971ead5aa6d43afeb651b89d2
GUILHERME SANTOS DA ROCHA <guilherme.srocha18@senacsp.edu.br>
2026-02-26 16:38:23 -0300

    trabalho em dupla

---
+++ Gerenciamento de estoque/Gerenciamento de estoque/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Gerenciamento_de_estoque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblEstoque = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblEstoque2 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEstoque
+            // 
+            this.lblEstoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstoque.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEstoque.Location = new System.Drawing.Point(262, 19);
+            this.lblEstoque.Name = "lblEstoque";
+            this.lblEstoque.Size = new System.Drawing.Size(243, 30);
+            this.lblEstoque.TabIndex = 0;
+            this.lblEstoque.Text = "ESTOQUE DE CREME";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(517, 126);
+            this.lblStatus.Multiline = true;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(273, 434);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "PRATELEIRA VAZIA! POR FAVOR REPOR OS PRODUTOS";
+            this.lblStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 568);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 24);
+            this.label4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(299, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 24);
+            this.label5.TabIndex = 9;          
+            // 
+            // lblEstoque2
+            // 
+            this.lblEstoque2.AutoSize = true;
+            this.lblEstoque2.Location = new System.Drawing.Point(562, 233);
+            this.lblEstoque2.Name = "lblEstoque2";
+            this.lblEstoque2.Size = new System.Drawing.Size(176, 13);
+            this.lblEstoque2.TabIndex = 10;
+            this.lblEstoque2.Text = "QUANTIDADE NO ESTOQUE: 100";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(556, 370);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(177, 20);
+            this.txtQuantidade.TabIndex = 12;
+            this.txtQuantidade.Text = "";            
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(560, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 43);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Digite a quantidade que deseja retirar do estoque\r\n";            
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(536, 419);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 71);
+            this.panel1.TabIndex = 14;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(584, 510);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(128, 28);
+            this.btnEnviar.TabIndex = 15;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(0, 560);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(803, 46);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(0, -3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(803, 71);
+            this.panel3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(788, 601);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.lblEstoque2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEstoque);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Gerenciamento de estoque";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEstoque;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lblStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblEstoque2;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Gerenciamento de estoque/Gerenciamento de estoque/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerenciamento_de_estoque
+{
+    public partial class Form1 : Form
+    {
+        // Quantidade inicial do estoque
+        int estoqueAtual = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AtualizarEstoque();
+        }
+
+        // Atualiza a quantidade exibida e o status do estoque
+        private void AtualizarEstoque()
+        {
+            lblEstoque2.Text = "QUANTIDADE NO ESTOQUE: " + estoqueAtual;
+
+            // se for menor que 5, mostra alerta
+            if (estoqueAtual < 5)
+            {
+                lblStatus.Text = "Alerta: Baixo estoque. Por favor, reabasteça este produto.";
+                lblStatus.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblStatus.Text = "Estoque suficiente.";
+                lblStatus.ForeColor = Color.Green;
+            }
+        }
+
+        // Exibe mensagens de operação no painel
+        private void ExibirMensagemNoPanel(string mensagem, Color cor)
+        {
+            panel1.Controls.Clear();
+
+            Label lblMensagem = new Label();
+            lblMensagem.Text = mensagem;
+            lblMensagem.ForeColor = cor;
+            lblMensagem.Font = new Font("Arial", 10, FontStyle.Bold);
+            lblMensagem.AutoSize = false;
+            lblMensagem.Size = panel1.Size;
+            lblMensagem.TextAlign = ContentAlignment.MiddleCenter;
+
+            panel1.Controls.Add(lblMensagem);
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtQuantidade.Text, out int quantidadeRetirada))
+            {
+                // Não aceita zero ou valores negativos
+                if (quantidadeRetirada <= 0)
+                {
+                    ExibirMensagemNoPanel("Insira uma quantidade maior que zero!", Color.OrangeRed);
+                    return;
+                }
+
+                // Não aceita retirada maior do que o estoque disponível
+                if (quantidadeRetirada > estoqueAtual)
+                {
+                    ExibirMensagemNoPanel("Não há essa quantidade disponível em estoque!", Color.OrangeRed);
+                    return;
+                }
+
+                // Faz a retirada
+                estoqueAtual -= quantidadeRetirada;
+
+                // Atualiza a tela com o novo valor do estoque
+                AtualizarEstoque();
+
+                // Mostra a mensagem da operação
+                ExibirMensagemNoPanel("Foram retiradas " + quantidadeRetirada + " unidades do estoque!", Color.Green);
+
+                // Limpa a caixa de texto
+                txtQuantidade.Clear();
+            }
+            else
+            {
+                ExibirMensagemNoPanel("Por favor, insira um número válido!", Color.OrangeRed);
+            }
+        }
+
+        // Métodos vazios ligados ao Designer
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void lblEstoque_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void txtQuantidade_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void txtQuantidade_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void lblStatus_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
